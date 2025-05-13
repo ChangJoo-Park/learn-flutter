7be4a9578ae55692f936f57a1cdbdcd2360d5a4c
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2025-05-13 18:03:49 +0900

    chore: update sidebars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,74 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  compressHTML: true,
+  prefetch: true,
+  trailingSlash: "always",
+  integrations: [
+    starlight({
+      title: "My Own Flutter Book",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "안내",
+          items: [
+            { label: "소개", slug: "introduction/introduction" },
+            { label: "시작하기", slug: "introduction/getting-started" },
+          ],
+        },
+        {
+          label: "튜토리얼",
+          items: [
+            { label: "프로젝트 1. 카운터", slug: "tutorials/1_counter" },
+            {
+              label: "프로젝트 2. TodoList",
+              slug: "tutorials/2_todo_list",
+            },
+          ],
+        },
+        {
+          label: "개발 가이드",
+          items: [
+            { label: "프로젝트 1. 카운터", slug: "tutorials/1_counter" },
+            {
+              label: "프로젝트 2. TodoList",
+              slug: "tutorials/2_todo_list",
+            },
+          ],
+        },
+        {
+          label: "주요 컴포넌트",
+          items: [
+            { label: "프로젝트 1. 카운터", slug: "tutorials/1_counter" },
+            {
+              label: "프로젝트 2. TodoList",
+              slug: "tutorials/2_todo_list",
+            },
+          ],
+        },
+        {
+          label: "참조",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "리소스",
+          items: [
+            {
+              label: "프로젝트 2. TodoList",
+              slug: "tutorials/2_todo_list",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
