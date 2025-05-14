5dfa13f8e265e7d14b21024400f82be2e3fcc64a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2025-05-14 12:14:36 +0900

    feat: update social

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/changjoo-park/learn-flutter",
         },
       ],
       sidebar: sidebars,
